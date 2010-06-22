9be4582a1d7828d7d15daff02d91cdaf66e5bcbf
Britton Ballard <davidbritton.ballard@gmail.com>
2010-06-21 21:53:48 -0500

    [#3975505] Added test for ensuring entitys can be parents to other entities

--- test/unit/entity_test.rb
+++ test/unit/entity_test.rb
@@ -13,4 +13,19 @@ class EntityTest < ActiveSupport::TestCase
   should_validate_presence_of(:system_identifier)
   should_validate_presence_of(:organization_id)
   should_validate_uniqueness_of(:name, :scoped_to => [:organization_id])
+  
+  context('Entity') do
+    setup do
+      @entity = Factory.build(:entity)
+    end
+    
+    should('allow entities to have child entites') do
+      @entity.save
+      assert_equal(0, @entity.children.length)
+      @entity.children << Factory.create(:entity)
+      assert_equal(1, @entity.children.length)
+      @entity.children << Factory.create(:entity)
+      assert_equal(2, @entity.children.length)
+    end
+  end
 end
