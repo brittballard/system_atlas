75838fc561042f7903cef3bcbb19421d84323e0c
Britton Ballard <davidbritton.ballard@gmail.com>
2011-01-16 17:40:14 -0600

    First run at adding some javascript drive ui/ux to the application.

--- public/javascripts/systemAtlas.views.js
+++ public/javascripts/systemAtlas.views.js
@@ -1,7 +1,14 @@
 systemAtlas.views = (function($){
   return {
     relatipnships_new : function(){
-
+      $( "#draggable" ).draggable();
+          $( "#droppable" ).droppable({
+            drop: function( event, ui ) {
+              $( this )
+                .find( "p" )
+                  .html( "Dropped!" );
+            }
+          });
     }
   }
 })(jQuery);
