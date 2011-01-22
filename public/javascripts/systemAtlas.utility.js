79a1e142ec53ccd50dc04fd06bb7b2551c2e1c84
Britton Ballard <davidbritton.ballard@gmail.com>
2011-01-22 14:54:51 -0600

    Added some meat to the ajax UI. Much work remains to be done.

--- public/javascripts/systemAtlas.utility.js
+++ public/javascripts/systemAtlas.utility.js
@@ -1,14 +1,8 @@
 systemAtlas.utility = (function($){
   return {
-    register_date_picker : function(date_pickers){
-      for(var i = 0; i < date_pickers.length; i++){
-        date_pickers[i].datepicker();
-      }
-    },
-    
-    set_data : function(element, data_defs){
-      for(var i = 0; i < data_defs.length; i++){
-        element.data(data_defs[i][0], data_defs[i][1]);
+    setData : function(element, dataDefs){
+      for(var i = 0; i < dataDefs.length; i++){
+        element.data(dataDefs[i][0], dataDefs[i][1]);
       }
     }
   }
--- public/javascripts/systemAtlas.views.js
+++ public/javascripts/systemAtlas.views.js
@@ -1,14 +1,21 @@
 systemAtlas.views = (function($){
   return {
-    relatipnships_new : function(){
-      $( "#draggable" ).draggable();
-          $( "#droppable" ).droppable({
-            drop: function( event, ui ) {
-              $( this )
-                .find( "p" )
-                  .html( "Dropped!" );
-            }
-          });
+    relatipnshipsNew : function(newRelationshipUrl, childEntityIds, parentEntityId){
+      for(var i = 0; i < childEntityIds.length; i++){
+        systemAtlas.utility.setData($(String.concat('#', childEntityIds[i][0])), [['id', childEntityIds[i][1]]])
+      }
+      
+      systemAtlas.utility.setData($(String.concat('#entity-', parentEntityId)), [['id', parentEntityId]])
+
+      $(".draggable").draggable();
+      
+      $(".droppable").droppable({
+        drop: function(event, ui) {
+          $('#relationships').append(String.concat("New Relationship entity with id ", ui.draggable.data("id"), ".<br>"));
+          $.post(newRelationshipUrl,
+            { parent: $(this).data('id'), child: ui.draggable.data("id") })
+          }
+      });
     }
   }
 })(jQuery);
