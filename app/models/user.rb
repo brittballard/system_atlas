06dfc7d0f0f1374582ea63d7c0a39975aa11d3b2
Britton Ballard <davidbritton.ballard@gmail.com>
2011-03-14 22:25:02 -0500

    Added person create on user registration.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   belongs_to :organization
+  after_save :create_person
   merge_validates_length_of_login_field_options :message => 'is too short. Minimum 3 characters required'
   
   acts_as_authentic
@@ -18,6 +19,11 @@ class User < ActiveRecord::Base
   
   def is?(role)
     roles.include?(role.to_s)
+  end
+  
+  def create_person
+    person = Person.new({ :email => self.email })
+    person.save
   end
   
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,10 @@
 require 'spec_helper'
 
 describe User do
+  describe 'creating a user' do
+    it 'should create a person after saving a user' do
+      new_user = Factory.create(:user)
+      Person.where('email = ?', new_user.email).count.should eql(1)
+    end
+  end
 end
