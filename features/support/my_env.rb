1a97e5957603834937e8c92a1b10f48ca968f888
Britton Ballard <davidbritton.ballard@gmail.com>
2011-09-01 00:51:08 -0500

    Still updating to Rails 3.1. Getting closer.

--- features/support/my_env.rb
+++ features/support/my_env.rb
@@ -1,5 +1,5 @@
 require 'factory_girl'
-Factory.definition_file_paths = [File.join(Rails.root, 'spec', 'factories')]
-Factory.find_definitions
+# FactoryGirl.definition_file_paths = [File.join(Rails.root, 'spec', 'factories')]
+# FactoryGirl.find_definitions
 require 'factory_girl/step_definitions'
 require 'ruby-debug'
