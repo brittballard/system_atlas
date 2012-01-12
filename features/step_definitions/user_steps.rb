a58e9541101150afadb9feb977a81c8c71a1b68a
Britton Ballard <davidbritton.ballard@gmail.com>
2012-01-11 20:41:01 -0600

    Updated stuff to make this thing work again.

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,11 +1,11 @@
 Given /^I am logged in as an? "([^"]*)"$/ do |role|
-  And %{I am logged in as a "#{role}" for organization_id 1000}
+  step %{I am logged in as a "#{role}" for organization_id 1000}
 end
 
 Given /^I am logged in as an? "([^"]*)" for organization_id (\d+)$/ do |role, organization_id|
   @user = Factory.create(:user, :roles_mask => ([role.downcase] & User::ROLES).map { |r| 2**User::ROLES.index(r) }.sum, :organization => Factory.create(:organization, :id => organization_id))
-  And %{I go to the login page}
-  And %{I fill in "user_email" with "#{@user.email}"}
-  And %{I fill in "user_password" with "#{@user.password}"}
-  And %{I press "Sign in"}
+  step %{I go to the login page}
+  step %{I fill in "user_email" with "#{@user.email}"}
+  step %{I fill in "user_password" with "#{@user.password}"}
+  step %{I press "Sign in"}
 end
