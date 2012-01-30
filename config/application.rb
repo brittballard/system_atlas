ff1641e3a4dd5c3c8ba908cd1387b84c9c42350b
Britton Ballard <davidbritton.ballard@gmail.com>
2012-01-29 21:47:56 -0600

    Trying to fix the style sheets with all this asset pipeline madness.

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,8 @@ module SystemAtlas
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-        
+    
+    config.assets.precompile += ['system_atlas.css']    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
