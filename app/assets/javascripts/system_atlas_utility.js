5761a9db16ceef81d06dd5dec33015e2af935676
Britton Ballard <davidbritton.ballard@gmail.com>
2013-05-22 20:28:23 -0700

    Add brief case icon to business unity entity partial.

--- app/assets/javascripts/system_atlas_utility.js
+++
@@ -1,9 +0,0 @@
-SystemAtlas.utility = (function($){
-  return {
-    setData : function(element, dataDefs){
-      for(var i = 0; i < dataDefs.length; i++){
-        element.data(dataDefs[i][0], dataDefs[i][1]);
-      }
-    }
-  }
-})(jQuery);
